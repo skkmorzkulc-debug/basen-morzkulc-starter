730ee7625dab06925ffa76561e76933a5b5782db
Marcin Gruby <marcingruby@MacBook-Air-Marcin.local>
2025-09-14 22:20:24 +0200

    Fix Firebase storageBucket (appspot.com)

--- src/firebase.ts
+++ src/firebase.ts
@@ -9,7 +9,8 @@ const firebaseConfig = {
   apiKey: "AIzaSyBDOI9ogqvefxEhzS_EHJ9EH1uEjeQgkuM",
   authDomain: "basen-morzkulc-7564a.firebaseapp.com",
   projectId: "basen-morzkulc-7564a",
-  storageBucket: "basen-morzkulc-7564a.firebasestorage.app",
+storageBucket: "basen-morzkulc-c2a81.appspot.com",
+
   messagingSenderId: "396983396222",
   appId: "1:396983396222:web:c79360de6acd53ea65f52f"
 };
