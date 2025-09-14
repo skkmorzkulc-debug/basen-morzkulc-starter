bc453e543dd0a2557f6d5bc242dd2106c75fd9b1
Marcin Gruby <marcingruby@MacBook-Air-Marcin.local>
2025-09-14 23:37:00 +0200

    Use VITE_* env in firebase.ts (remove hardcoded project)

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,21 +1,14 @@
-// UZUPEŁNIJ TĘ KONFIGURACJĘ DANYMI Z Firebase Console -> Project settings -> Your apps (Web)
-// window._FIREBASE_CONFIG lub firebaseConfig obiekt
 import { initializeApp } from 'firebase/app'
-import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
-import { getFunctions } from 'firebase/functions'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBDOI9ogqvefxEhzS_EHJ9EH1uEjeQgkuM",
-  authDomain: "basen-morzkulc-7564a.firebaseapp.com",
-  projectId: "basen-morzkulc-7564a",
-storageBucket: "basen-morzkulc-c2a81.appspot.com",
-
-  messagingSenderId: "396983396222",
-  appId: "1:396983396222:web:c79360de6acd53ea65f52f"
-};
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+}
 
 export const app = initializeApp(firebaseConfig)
-export const auth = getAuth(app)
 export const db = getFirestore(app)
-export const functions = getFunctions(app, 'europe-central2') // Polska strefa
